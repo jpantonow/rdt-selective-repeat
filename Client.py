0113f0e741d7c0913bbb3b108360b0254d5dd125
jpantonov <jpantonow@gmail.com>
2023-11-23 20:53:16 -0300

    goodput

--- Client.py
+++ Client.py
@@ -64,11 +64,12 @@ if __name__ == '__main__':
         'The art of debugging is figuring out what you really told your program to do rather than what you thought you told it to do. -- Andrew Singer',
         'The computer was born to solve problems that did not exist before. - Bill Gates']
     
-    for i in range(0,5): #pra tornar mais extenso 
+    for i in range(0,1): #pra tornar mais extenso 
         messages.append(msg_L)
     timeout = 1000  # send the next message if not response
     rdt = RDT.RDT('client', args.server, args.port)
     try:
+        t1 = time.time()
         for msg_L in messages:
             for message in msg_L:
                     time_of_last_data = time.time()
@@ -78,7 +79,8 @@ if __name__ == '__main__':
             rdt.rdt_4_0_send(msg_L)
             rdt.clear()
             #dar um jeito de imprimir as mensagens
-    
+        t2 = time.time()
+        print(f"Goodput=={(t1-t2):.2f}[s]")
     except (KeyboardInterrupt, SystemExit):
         print("Ending connection...")
     except (BrokenPipeError, ConnectionAbortedError, ConnectionResetError):
